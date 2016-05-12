aa42a3e30ccb2f1d3a59bd6d1c827e2a8eeceda0
Alan Tsai <alan@alantsai.net>
2016-05-12 22:55:11 +0800

    加入 .net 45 library專案

---
+++ src/ReusableLibrary/Net45Library/UsefulLibrary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net45Library
+{
+    public static class UsefulLibrary
+    {
+        public static void PrintLibraryVersion()
+        {
+            string version = Assembly
+                     .GetExecutingAssembly()
+                     .GetReferencedAssemblies()
+                     .Where(x => x.Name == "System.Core").First().Version.ToString();
+
+            Console.WriteLine(version);
+        }
+    }
+}
