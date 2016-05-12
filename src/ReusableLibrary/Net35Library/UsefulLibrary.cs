ef6c65a4594ddd781b7a8a40287f1f19e7f29363
Alan Tsai <alan@alantsai.net>
2016-05-12 22:57:27 +0800

    加入 .net 35 library專案

---
+++ src/ReusableLibrary/Net35Library/UsefulLibrary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Net35Library
+{
+    public static class UsefulLibrary
+    {
+        public static void PrintLibraryVersionFor35()
+        {
+            string version = Assembly
+                     .GetExecutingAssembly()
+                     .GetReferencedAssemblies()
+                     .Where(x => x.Name == "System.Core").First().Version.ToString();
+
+            Console.WriteLine(version);
+        }
+    }
+}
